27e559ed4a3a6599c4f588c45aa2618435bf9fab
Queenie <qthamshinee@gmail.com>
2025-06-18 13:30:19 +0800

    Delete download.js

--- download.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('downloadBtn').addEventListener('click', function () {
-  const link = document.createElement('a');
-  link.href = 'sample.pdf'; 
-  link.download = 'DownloadedFile.pdf'; 
-  link.click();
-});
